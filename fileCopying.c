bbfb7f782a3246f6c36e9c2eb10d78b3d1dc3593
squishybaker <grrtt.slmn@gmail.com>
2023-09-28 10:22:38 -0700

    general

---
+++ fileCopying.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* copy input to output; 1st version */
+
+int main(){
+    int c;
+
+    printf("Enter some characters (Press Ctrl+C to finish:)\n");
+    
+    while ((c = getchar()) != EOF){
+        putchar(c);
+    }
+    
+}
