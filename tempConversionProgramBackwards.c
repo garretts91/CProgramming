67d889ba9ff1f85f8c722f6c4b9f821b9fb8e21a
squishybaker <grrtt.slmn@gmail.com>
2023-09-28 18:58:14 -0700

    general

---
+++ tempConversionProgramBackwards.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() /* Fahrenheit-Celsius table */
+{
+    int fahr;
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+        printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
