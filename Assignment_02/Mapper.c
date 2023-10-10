9f8945ac2eef6531388bcfea198935a119ffa11d
squishybaker <grrtt.slmn@gmail.com>
2023-10-10 11:43:45 -0700

    general

---
+++ Assignment_02/Mapper.c
@@ -0,0 +1,32 @@
+﻿#include "Mapper.h"
+#include <stdio.h>
+
+void OutputAsHexCharacters(unsigned int value)
+{
+	const int NIBBLES_PER_BYTE = 2;
+	const int NIBBLE_COUNT = sizeof(unsigned int) * NIBBLES_PER_BYTE;
+	const int BITS_PER_NIBBLE = 4;
+
+	unsigned int mask = 0xF0000000;
+
+	printf("\n");
+
+	for (int count = 0; count < NIBBLE_COUNT; count++)
+	{
+		
+
+
+
+
+	}
+	
+	printf("\n");
+}
+
+unsigned char MapNibbleToHexCharacter(unsigned int value)
+{
+	unsigned char lookUpTable[] = {'0', '1', '2', '3', '4', '5', '6', '7', 
+							 '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+	return lookUpTable[value];
+}
---
+++ Assignment_02/Program.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include "Mapper.h"
+
+int main() 
+{
+   
+	//Input
+	unsigned int value = 0;
+	
+	printf("Enter an unsigned integer value: ");
+	int returnValue = scanf("%d", &value);
+
+	//Process & Output
+	OutputAsHexCharacters(value);
+
+	printf("Press the enter key to continue...");
+
+	int c = getchar();
+	c = getchar();
+
+	return 0;
+}
