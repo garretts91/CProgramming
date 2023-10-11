07bed998fada4833b1d71715a598244660cc7202
squishybaker <grrtt.slmn@gmail.com>
2023-10-11 10:18:26 -0700

    general

--- bitManipulation/Mapper.c
+++ bitManipulation/Mapper.c
@@ -4,6 +4,7 @@
 void OutputAsHexCharacters(unsigned int value)
 {
     const int NIBBLES_PER_BYTE = 2;
+    // NIBBLE_COUNT: the size of an unsigned int in C is 4, and there are 2 nibbles per byte, making the nibble count 8;
     const int NIBBLE_COUNT = sizeof(unsigned int) * NIBBLES_PER_BYTE;
     const int BITS_PER_NIBBLE = 4;
 
@@ -13,6 +14,8 @@ void OutputAsHexCharacters(unsigned int value)
     printf("\n");
 
     for (int count = 0; count < NIBBLE_COUNT; count++) {
+        // shiftAmount calculates the index of the nibble to extract from value
+        // shifts the nibbles from most significant bit to least significant bit (since count = 0 in for loop and we are count++)
         int shiftAmount = (NIBBLE_COUNT - 1 - count) * BITS_PER_NIBBLE;
         unsigned int nibble = (value & mask) >> shiftAmount;
         printf("The value of nibble is: 0x%02X\n", nibble);
