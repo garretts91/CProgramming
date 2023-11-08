7be67ac43aa7c2618ce265ca02ada7506e058003
squishybaker <grrtt.slmn@gmail.com>
2023-11-07 20:52:03 -0800

    general

--- SISA_Assembler/Program.c
+++ SISA_Assembler/Program.c
@@ -7,6 +7,7 @@ void main()
     TestAssemblerInstruction("Assembly Instruction: LDR_STR", "AssemblyCodeInstructions_02.aci", "MachineCodeInstructions_02.mci", "MachineCodeInstructions_02_Valid.mci");
     TestAssemblerInstruction("Assembly Instruction: LDR_STR", "AssemblyCodeInstructions_03.aci", "MachineCodeInstructions_03.mci", "MachineCodeInstructions_03_Valid.mci");
     TestAssemblerInstruction("Assembly Instruction: CMP_BL_B", "AssemblyCodeInstructions_04.aci", "MachineCodeInstructions_04.mci", "MachineCodeInstructions_04_Valid.mci");
+    TestAssemblerInstruction("Assembly Insutrction: totalArray", "totalArray.aci", "totalArray.mci", "MachineCodeInstructions_04_Valid.mci");
 
     printf("Press the enter key to continue...");
 
